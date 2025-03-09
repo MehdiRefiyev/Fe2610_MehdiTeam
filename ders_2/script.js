8a6f2a0fed618147c49965c92b8dc2e6139a85ba
Mehdi <mehdirefiyev177@gmail.com>
2025-03-09 13:57:51 +0400

    second commit

---
+++ ders_2/script.js
@@ -0,0 +1,10 @@
+const { aut : [a,b] } = { aut : ["ibra" , "Kenan"]};
+
+// array burda metofirikdi
+// const [a, b] = array;
+// const [a, , b] = array;
+// const [a = aDefault, b] = array;
+// const [a, b, ...rest] = array;
+// const [a, , b, ...rest] = array;
+// const [a, b, ...{ pop, push }] = array;
+// const [a, b, ...[c, d]] = array;
